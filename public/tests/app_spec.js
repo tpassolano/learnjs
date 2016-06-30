13443426261dfc83c1c2e07c33adbb7505ebab73
Tony Passolano <tpassolano@gmail.com>
2016-06-29 20:33:06 -0700

    Chapter 3. Essentials of Single Page Application - Handling User Input
    Added Form for user to supply answer to question.

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -27,11 +27,20 @@ describe('LearnJS', function() {
         $(window).trigger('hashchange');
         expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
     });
+   
 
-    describe('problem view', function() {
-        it('has a title that includes the problem number', function() {
-            var view = learnjs.problemView('1');
-            expect(view.text()).toEqual('Problem #1 Coming soon!');
-        });
-    });
 });
+
+describe('answer section', function() { 
+        it('can check a correct answer by hitting a button', function() { 
+            view.find('.answer').val('true'); 
+            view.find('.check-btn').click(); 
+            expect(view.find('.result').text()).toEqual('Correct!'); 
+        });
+
+        it('rejects an incorrect answer', function() { 
+            view.find('.answer').val('false'); 
+            view.find('.check-btn').click(); 
+            expect(view.find('.result').text()).toEqual('Incorrect!'); 
+        }); 
+    });
