bf830dac32ee8b28891282fe8fe45a8d483a1530
Tony Passolano <tony@PIC>
2016-06-29 17:08:36 -0700

    Chapter 2 Routing Views with Hash Events - Adding View Parameters
    Separate the route out by the dash.

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -3,8 +3,15 @@ describe('LearnJS', function() {
         learnjs.showView('#problem-1');
         expect($('.view-container .problem-view').length).toEqual(1);
     });
+    
     it('shows the landing page view when there is no hash', function() {
         learnjs.showView('');
         expect($('.view-container .landing-view').length).toEqual(1);
+    });
+
+    it('passes the hash view parameter to the view function', function() {
+        spyOn(learnjs, 'problemView');
+        learnjs.showView('#problem-42');
+        expect(learnjs.problemView).toHaveBeenCalledWith('42');
     });
 });
