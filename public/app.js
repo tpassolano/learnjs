f1060fb8de41c4a84716df2de5d533aae3bc2e4e
Tony Passolano <tpassolano@gmail.com>
2016-06-30 19:46:42 -0700

    Chapter 3. Essentials of Single Page Apps - Controlling Navigation

--- public/app.js
+++ public/app.js
@@ -21,47 +21,59 @@ learnjs.problems = [
   }
 ];
 
-learnjs.applyObject = function(obj, elem) {
+learnjs.applyObject = function (obj, elem) {
   for (var key in obj) {
     elem.find('[data-name="' + key + '"]').text(obj[key]);
   }
 };
 
-learnjs.problemView = function(data) {
+learnjs.problemView = function (data) {
   var problemNumber = parseInt(data, 10);
   var view = $('.templates .problem-view').clone();
-  var problemData = learnjs.problems[problemNumber - 1]; 
-  var resultFlash = view.find('.result'); 
+  var problemData = learnjs.problems[problemNumber - 1];
+  var resultFlash = view.find('.result');
 
-  function checkAnswer() { 
+  function checkAnswer() {
     var answer = view.find('.answer').val();
     var test = problemData.code.replace('__', answer) + '; problem();';
     return eval(test);
   }
 
-  learnjs.flashElement = function(elem, content) { 
-  elem.fadeOut('fast', function() { 
-    elem.html(content); 
-    elem.fadeIn(); 
-  }); 
-}
+  learnjs.flashElement = function (elem, content) {
+    elem.fadeOut('fast', function () {
+      elem.html(content);
+      elem.fadeIn();
+    });
+  }
+ 
 
-  function checkAnswerClick() { 
+  function checkAnswerClick() {
     if (checkAnswer()) {
-      learnjs.flashElement(resultFlash, 'Correct!');
-    } else {
-      learnjs.flashElement(resultFlash, 'Incorrect!');
-    }
-    return false;
+      var correctFlash = learnjs.template('correct-flash');
+      correctFlash.find('a').attr('href', '#problem-' + (problemNumber + 1));
+      learnjs.flashElement(resultFlash, learnjs.buildCorrectFlash(problemNumber));      
+    } else { learnjs.flashElement(resultFlash, 'Incorrect!'); } return false;
   }
 
-  view.find('.check-btn').click(checkAnswerClick); 
+  learnjs.buildCorrectFlash = function (problemNum) {
+    var correctFlash = learnjs.template('correct-flash');
+    var link = correctFlash.find('a');
+    if (problemNum < learnjs.problems.length) {
+      link.attr('href', '#problem-' + (problemNum + 1));
+    }
+    else {
+      link.attr('href', ''); link.text("You're Finished!");
+    } return correctFlash;
+  }
+
+
+  view.find('.check-btn').click(checkAnswerClick);
   view.find('.title').text('Problem #' + problemNumber);
   learnjs.applyObject(problemData, view);
   return view;
 }
 
-learnjs.showView = function(hash) {
+learnjs.showView = function (hash) {
   var routes = {
     '#problem': learnjs.problemView
   };
@@ -72,8 +84,10 @@ learnjs.showView = function(hash) {
   }
 }
 
-learnjs.appOnReady = function() {
-  window.onhashchange = function() {
+learnjs.template = function (name) { return $('.templates .' + name).clone(); }
+
+learnjs.appOnReady = function () {
+  window.onhashchange = function () {
     learnjs.showView(window.location.hash);
   };
   learnjs.showView(window.location.hash);
