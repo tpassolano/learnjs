705bed9b89cb94c68c4a7c24f711bf79d8418472
Tony Passolano <tony@PIC>
2016-07-07 13:25:34 -0700

    Chapter 4. Identity as a Service with Amason Cognito - Creating a Profile View

--- public/app.js
+++ public/app.js
@@ -1,3 +1,4 @@
+  
 /***
  * Excerpted from "Serverless Single Page Apps",
  * published by The Pragmatic Bookshelf.
@@ -25,15 +26,48 @@ learnjs.problems = [
   }
 ];
 
-learnjs.applyObject = function (obj, elem) {
+learnjs.triggerEvent = function(name, args) {
+  $('.view-container>*').trigger(name, args);
+}
+
+learnjs.template = function(name) {
+  return $('.templates .' + name).clone();
+}
+
+learnjs.applyObject = function(obj, elem) {
   for (var key in obj) {
     elem.find('[data-name="' + key + '"]').text(obj[key]);
   }
 };
 
-learnjs.problemView = function (data) {
+learnjs.addProfileLink = function(profile) {
+  var link = learnjs.template('profile-link');
+  link.find('a').text(profile.email);
+  $('.signin-bar').prepend(link);
+}
+
+learnjs.flashElement = function(elem, content) {
+  elem.fadeOut('fast', function() {
+    elem.html(content);
+    elem.fadeIn();
+  });
+}
+
+learnjs.buildCorrectFlash = function (problemNum) {
+  var correctFlash = learnjs.template('correct-flash');
+  var link = correctFlash.find('a');
+  if (problemNum < learnjs.problems.length) {
+    link.attr('href', '#problem-' + (problemNum + 1));
+  } else {
+    link.attr('href', '');
+    link.text("You're Finished!");
+  }
+  return correctFlash;
+}
+
+learnjs.problemView = function(data) {
   var problemNumber = parseInt(data, 10);
-  var view = $('.templates .problem-view').clone();
+  var view = learnjs.template('problem-view');
   var problemData = learnjs.problems[problemNumber - 1];
   var resultFlash = view.find('.result');
 
@@ -43,33 +77,24 @@ learnjs.problemView = function (data) {
     return eval(test);
   }
 
-  learnjs.flashElement = function (elem, content) {
-    elem.fadeOut('fast', function () {
-      elem.html(content);
-      elem.fadeIn();
-    });
-  }
- 
-
   function checkAnswerClick() {
     if (checkAnswer()) {
-      var correctFlash = learnjs.template('correct-flash');
-      correctFlash.find('a').attr('href', '#problem-' + (problemNumber + 1));
-      learnjs.flashElement(resultFlash, learnjs.buildCorrectFlash(problemNumber));      
-    } else { learnjs.flashElement(resultFlash, 'Incorrect!'); } return false;
-  }
-
-  learnjs.buildCorrectFlash = function (problemNum) {
-    var correctFlash = learnjs.template('correct-flash');
-    var link = correctFlash.find('a');
-    if (problemNum < learnjs.problems.length) {
-      link.attr('href', '#problem-' + (problemNum + 1));
+      var flashContent = learnjs.buildCorrectFlash(problemNumber);
+      learnjs.flashElement(resultFlash, flashContent);
+    } else {
+      learnjs.flashElement(resultFlash, 'Incorrect!');
     }
-    else {
-      link.attr('href', ''); link.text("You're Finished!");
-    } return correctFlash;
+    return false;
   }
 
+  if (problemNumber < learnjs.problems.length) {
+    var buttonItem = learnjs.template('skip-btn');
+    buttonItem.find('a').attr('href', '#problem-' + (problemNumber + 1));
+    $('.nav-list').append(buttonItem);
+    view.bind('removingView', function() {
+      buttonItem.remove();
+    });
+  }
 
   view.find('.check-btn').click(checkAnswerClick);
   view.find('.title').text('Problem #' + problemNumber);
@@ -77,7 +102,19 @@ learnjs.problemView = function (data) {
   return view;
 }
 
-learnjs.showView = function (hash) {
+learnjs.landingView = function() {
+  return learnjs.template('landing-view');
+}
+
+learnjs.profileView = function() {
+  var view = learnjs.template('profile-view');
+  learnjs.identity.done(function(identity) {
+    view.find('.email').text(identity.email);
+  });
+  return view;
+}
+
+learnjs.showView = function(hash) {
   var routes = {
     '#problem': learnjs.problemView,
     '#profile': learnjs.profileView,
@@ -87,50 +124,17 @@ learnjs.showView = function (hash) {
   var hashParts = hash.split('-');
   var viewFn = routes[hashParts[0]];
   if (viewFn) {
+    learnjs.triggerEvent('removingView', []);
     $('.view-container').empty().append(viewFn(hashParts[1]));
   }
 }
 
-learnjs.template = function (name) 
-{ 
-  return $('.templates .' + name).clone();
-}
-
-learnjs.landingView = function(){
-  return learnjs.template('landing-view');
-}
-
-learnjs.appOnReady = function () {
-  window.onhashchange = function () {
+learnjs.appOnReady = function() {
+  window.onhashchange = function() {
     learnjs.showView(window.location.hash);
   };
   learnjs.showView(window.location.hash);
   learnjs.identity.done(learnjs.addProfileLink);
-}
- 
-
-function googleSignIn(googleUser) {
-  var id_token = googleUser.getAuthResponse().id_token;
-    AWS.config.update({
-      region: 'us-east-1',
-      credentials: new AWS.CognitoIdentityCredentials({
-      IdentityPoolId: learnjs.poolId,
-      Logins: {
-      'accounts.google.com': id_token
-      }
-    })
-  })
-}
-
-function refresh() {
-  return gapi.auth2.getAuthInstance().signIn({
-      prompt: 'login'
-    }).then(function(userUpdate) {
-      var creds = AWS.config.credentials;
-      var newToken = userUpdate.getAuthResponse().id_token;
-      creds.params.Logins['accounts.google.com'] = newToken;
-    return learnjs.awsRefresh();
-  });
 }
 
 learnjs.awsRefresh = function() {
@@ -145,24 +149,32 @@ learnjs.awsRefresh = function() {
   return deferred.promise();
 }
 
-learnjs.awsRefresh().then(function(id) {
-  learnjs.identity.resolve({
-    id: id,
-    email: googleUser.getBasicProfile().getEmail(),
-    refresh: refresh
+function googleSignIn(googleUser) {
+  var id_token = googleUser.getAuthResponse().id_token;
+  AWS.config.update({
+    region: 'us-east-1',
+    credentials: new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: learnjs.poolId,
+      Logins: {
+        'accounts.google.com': id_token
+      }
+    })
+  })
+  function refresh() {
+    return gapi.auth2.getAuthInstance().signIn({
+        prompt: 'login'
+      }).then(function(userUpdate) {
+      var creds = AWS.config.credentials;
+      var newToken = userUpdate.getAuthResponse().id_token;
+      creds.params.Logins['accounts.google.com'] = newToken;
+      return learnjs.awsRefresh();
+    });
+  }
+  learnjs.awsRefresh().then(function(id) {
+    learnjs.identity.resolve({
+      id: id,
+      email: googleUser.getBasicProfile().getEmail(),
+      refresh: refresh
+    });
   });
-});
-
-learnjs.profileView = function() {
-  var view = learnjs.template('profile-view');
-  learnjs.identity.done(function(identity) {
-  view.find('.email').text(identity.email);
-  });
-  return view;
-}
-
-learnjs.addProfileLink = function(profile) {
-  var link = learnjs.template('profile-link');
-  link.find('a').text(profile.email);
-  $('.signin-bar').prepend(link);
 }
